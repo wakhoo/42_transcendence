720cb95f34f831877073208f0e920f533e0e73a7
mignoncode <mignoncode@macbookair.home>
2026-06-08 12:29:20 +0200

    nginx + docker-compose.yml update

---
+++ srcs/nginx/tools/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p /etc/nginx/certs
+
+if [ ! -f "/etc/nginx/certs/cert.pem" ]; then
+    openssl req -x509 -nodes \
+    -days 365 \
+    -newkey rsa:2048 \
+    -keyout /etc/nginx/certs/key.pem \
+    -out /etc/nginx/certs/cert.pem \
+    -subj "/C=FR/L=Mulhouse/O=42/OU=student/CN=dancel.42.fr"
+fi
+
+exec nginx -g "daemon off;"
