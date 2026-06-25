3f5a0ab961c9f42641baa009279441405d7063c5
Ahmed Boutaleb <aboutale@k1r3p16.42mulhouse.fr>
2026-06-25 21:30:43 +0200

    start loop game more

--- srcs/backend/src/game/game.gateway.ts
+++ srcs/backend/src/game/game.gateway.ts
@@ -1,37 +1,151 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ cors: true})
 
 export class GameGateway {
   
-    tab : string[] = [];
+/* Fonction pour Dorian permettant de stocker chaque client se connectant et deconnectant etc*/
 
-    handleConnection(client: any){
 
-      this.tab.push(client.id);
-      console.log("conexion : " , client.id);
-      console.log("joueur actuel " ,this.tab);
+  //   tab : string[] = [];
+  //   secretWord : string = "secret";
+    
+
+  //   handleConnection(client: any){
+
+  //     this.tab.push(client.id);
+  //     console.log("conexion : " , client.id);
+  //     console.log("joueur actuel " ,this.tab);
       
-    }
+  //   }
 
-    handleDisconnect(client: any) {
+  //   handleDisconnect(client: any) {
 
-      this.tab = this.tab.filter(id => id !== client.id);
-      console.log("deconnexion " , client.id);
-      console.log("joueur deco " ,this.tab);
+  //     this.tab = this.tab.filter(id => id !== client.id);
+  //     console.log("deconnexion " , client.id);
+  //     console.log("joueur deco " ,this.tab);
+  //   }
+
+  //   @SubscribeMessage('draw')
+  //   handleDraw(client: any, donnees : any) {
+
+  // console.log(`✏️ Le joueur ${client.id} vient de dessiner :`, donnees);
+
+  //     client.broadcast.emit('draw', donnees);
+
+  //   }
+
+
+
+  // debut de la boucle du jeu declaratio nde variable globale utilsiable dans toutes les fonctions 
+    @WebSocketServer()
+    server!: Server;
+    secretWord: string = "";
+    spectatorId : string[] = [];
+    timer: any;
+    timeLeft: number = 0;
+    wordList : string[] = ['pomme', 'television', 'parachute', 'voiture', 'Dorian', 'harmonica'];
+    currentDrawer: string = "";
+    playerList: string[] = [];
+    currentPlayerIndex: number = 0;
+    historicDraw : string[] = [];
+    playerPoints: Record<string,number> = {};
+
+    //chaque decorateur a utilsier avant sa fonction 
+    //rappel decorateur etiquette intelligente ici subscribe message un ecouteur start game qui recueprera les doneee adequates
+    @SubscribeMessage('start_game')
+    handleGame(client: any , data: any) {
+
+      console.log('Game started ');
+
+      // random pour choix du mot secret a chaque tour stockage du joueur numero un qui dessine 
+      
+      this.secretWord = this.wordList[Math.floor(Math.random() * this.wordList.length)];
+      this.currentDrawer = data.players[0];
+      this.playerList = data.players;
+      console.log(this.secretWord);
+      console.log("Dessinateur :", this.currentDrawer);
+     
+
+      //on donne un indice a tout les autres joueur la taille du mot
+      this.server.emit('word_hint' , {
+
+        drawer : this.currentDrawer,
+        wordLength: this.secretWord.length
+      });
+
+
+      // on donne le mot secret au dessinateur
+     
+        this.server.to(this.currentDrawer).emit('secret_word', this.secretWord);
+
+      // tableau d historique de dessin mis a null pour stocker chaque dessi nen temps reel a recueprer niveau front
+      this.historicDraw = [];
+      this.timeLeft = 60;
+      this.timer = setInterval(() => {
+
+        // debut de boucle de temps decrementation sur 60 s
+        this.timeLeft -= 1;
+        console.log(`Temps restant : ${this.timeLeft}s`);
+
+        if(this.timeLeft == 0){
+
+          clearInterval(this.timer);
+          console.log('Fin du temps reglementaire');
+        }
+     },1000);
     }
 
     @SubscribeMessage('draw')
-    handleDraw(client: any, donnees : any) {
+    handleDraw(client: any, drawData : any) { 
 
-  console.log(`✏️ Le joueur ${client.id} vient de dessiner :`, donnees);
-
-      client.broadcast.emit('draw', donnees);
+      // recuperation du dessin push dans tab si le dessinateur dessine
+      if(client.id === this.currentDrawer){
+          this.server.emit('draw', drawData);
+          this.historicDraw.push(drawData);
+          console.log(this.historicDraw.length);
+      }
+      //si spectateur essaye de dessiner message d erreur
+      else
+        console.log('Spectateur essaye de dessiner ')
 
     }
 
+    @SubscribeMessage('request_history')
+    handleRequest(client: any, drawData: any) {
 
-}
+        client.emit('request_history', this.historicDraw);
+    }
+
+    // fonction de verification du mot proposer si ces le bon attribution des points remise a zero du timer
+    
+    @SubscribeMessage('wordCheck')
+    handleWordFinding(client: any, wordProposition: any){
 
 
+      if(client.id === this.currentDrawer || this.spectatorId.includes(client.id))
+          return;
+      if(wordProposition === this.secretWord) {
 
+        if(!this.playerPoints[client.id])
+            this.playerPoints[client.id] = 0;
+
+          clearInterval(this.timer);
+
+          if(this.timeLeft >= 50)
+            this.playerPoints[client.id] += 100;
+          else if(this.timeLeft >= 40 )
+            this.playerPoints[client.id] += 80;
+          else if(this.timeLeft >= 30)
+            this.playerPoints[client.id] += 60;
+          else if(this.timeLeft >= 20 )
+            this.playerPoints[client.id] += 40;
+          else if(this.timeLeft >= 10 )
+            this.playerPoints[client.id] += 20;
+          else if(this.timeLeft > 0)
+            this.playerPoints[client.id] += 5;
+          this.server.emit('chat_message', `${client.id} a trouver le mot`);
+          }
+      }
+  }
