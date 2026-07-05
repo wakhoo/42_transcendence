3ff705b83c35614d2bdb1e6d480164e6fa0926f2
Xasiyy <asiya040906@gmail.com>
2026-07-05 11:38:17 +0200

    feat: Implement user login page

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
 
 function App() {
   return (
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
