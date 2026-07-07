0ad19d2500976ae23ba6695790fcb539b9fb4741
wakhoo <hello@chaewon.dev>
2026-07-07 13:45:19 +0000

    crash problem fixed

--- srcs/backend/src/chat/bad-words.seed.ts
+++ srcs/backend/src/chat/bad-words.seed.ts
@@ -4,14 +4,14 @@ export const BAD_WORDS = [
     'batarde', 'enculé', 'enculer', 'fdp', 'ta gueule', 'tg',
     'niquer', 'nique', 'baise', 'baiser', 'pd', 'pédé', 'ntm', 'nique ta mere',
     'con', 'conne', 'chier', 'chieur', 'chiasse', 'couillon', 'branleur', 'branler',
-    'pédophile', 'violeur', 'viol', 'pute',
-    'salopard', 'ordure', 'racaille', 'bâtard', 'tocard', 'abruti',
+    'pédophile', 'violeur', 'viol',
+    'salopard', 'ordure', 'racaille', 'tocard', 'abruti',
     'abrutie', 'débile', 'crétin', 'crétine', 'idiot', 'idiote', 'imbécile',
 
     // English
     'fuck', 'fucker', 'fucking', 'shit', 'bitch', 'asshole', 'ass',
     'cunt', 'bastard', 'damn', 'cock', 'dick', 'pussy', 'whore', 'slut',
-    'nigger', 'nigga', 'faggot', 'retard', 'rape', 'rapist', 'pedophile',
+    'nigger', 'nigga', 'faggot', 'retard', 'rape', 'rapist',
     'motherfucker', 'motherfucking', 'bullshit', 'jackass', 'dumbass',
-    'prick', 'twat', 'wanker', 'moron', 'idiot',
+    'prick', 'twat', 'wanker', 'moron',
 ];
