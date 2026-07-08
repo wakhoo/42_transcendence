1317ba2be7a464e1cb92460798ab69fb55230c4f
wakhoo <hello@chaewon.dev>
2026-07-08 10:32:05 +0000

    user field data exclude(passHash, totpSecret)

--- srcs/backend/src/main.ts
+++ srcs/backend/src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix("api");
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(3000, "0.0.0.0");
   console.log(`Backend up on http://0.0.0.0:3000/api`);
