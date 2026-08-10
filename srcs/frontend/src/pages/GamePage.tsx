0f91d88486c45990679e58acc59001a8f0840069
wakhoo <hello@chaewon.dev>
2026-08-10 15:29:16 +0000

    fix Game room UI -  pages/GamePage.tsx was holding <Timer /> with w-screen h-screen overflow-hidden min-w-[900px] it fixed the screen size so make it not responsible to the other size of screens

--- srcs/frontend/src/pages/GamePage.tsx
+++ srcs/frontend/src/pages/GamePage.tsx
@@ -1,16 +1,5 @@
 import Timer from '../components/Timer';
 
 export default function GamePage() {
-  
-  return (
-    <div className="relative w-screen h-screen bg-slate-100 overflow-hidden min-w-[900px]">
-    <h1 className="absolute top-5 left-8 text-4xl font-black text-slate-800 tracking widest uppercase 
-    drop-shadow-[3px_3px_0_rgba(46,204,113,0.4)]">
-      </h1>
-      <Timer />
-
-      {}
-      
-    </div>
-  );
+  return <Timer />;
 }
