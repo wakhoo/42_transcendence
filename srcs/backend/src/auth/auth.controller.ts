90dc80c3bca394dc9f0b12818324d1a1840d56ab
wakhoo <hello@chaewon.dev>
2026-08-20 05:32:19 +0000

    add throttle

--- srcs/backend/src/auth/auth.controller.ts
+++ srcs/backend/src/auth/auth.controller.ts
@@ -23,6 +23,7 @@ export class AuthController {
     ) {}
 
     @Post('register')
+    @Throttle({ default: { limit: 5, ttl: 60_000 } })
     register(@Body() dto: RegisterDto) {
         return this.authService.register(dto.email, dto.username, dto.password);
     }
